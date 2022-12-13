1ff3431c77cc92084af13657261abdc9b68f2f3f
Aundrei Bryant <drei97@gmail.com>
2022-12-13 12:40:39 -0600

    create an array function and map it

--- app/index.js
+++ app/index.js
@@ -1,2 +1,17 @@
-import user from "./user.js";
-console.log(user);
+import bios from "./users.js";
+
+function createBioCard(bio) {
+  return `
+    <p>${bio.name} <br>${bio.username} <br> ${bio.email}<br> from ${bio.city}<br> ${bio.phone} <br> ${bio.catchPhrase}  </p>
+  `;
+}
+
+const firstBioCard = createBioCard(bios[0]);
+console.log(bios);
+
+// const allNewBioCards = bios.map(firstBioCard);
+console.log(firstBioCard);
+
+const allBioCards = bios.map(createBioCard);
+
+console.log(allBioCards);
